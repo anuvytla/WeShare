df6529c7a89fcbc44463bc90c37743c477be1e4b
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-08-29 23:49:40 -0700

    add routes for user and thoughts

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  return res.send("Send the correct Route!");
+});
+
+module.exports = router;
