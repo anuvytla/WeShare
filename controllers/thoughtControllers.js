fe2f785a71410c3997dbba624aa33270bbcec782
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-08-31 13:58:15 -0700

    create delete thought, get singlethought and get thoughts functionality

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -51,14 +51,64 @@ const updateThought = async (req,res) => {
 }
 
 
+const deleteThought = async (req,res) => {
+    try {
+        let thoughtData = await Thoughts.findOneAndRemove({ _id: req.params.thoughtId });
+        let deletedThought = await Users.findOneAndUpdate(
+            { 
+                thoughts: req.params.thoughtId 
+            },
+            {
+                 $pull: { thoughts: req.params.thoughtId } 
+            },
+            { 
+                new: true 
+            }
+          );
+          if (!thoughtData) {
+            res.status(404).json({ message: "No thought found with this id!" });
+          }
+          if (!deleteThought) {
+            res.status(404).json({message: 'Thought created successfully. but no user found with this id!'})
+          }
+          res.status(200).json(deleteThought);
+    }catch (error) {
+        res.status(500).json(error);
+    }
+};
 
-module.exports = {createThought, updateThought,  }
+const getThoughts = async(req,res) => {
+    try{
+        let thoughtData = await Thoughts.find()
+        .sort(
+            { createdAt: -1 }
+            )
+        
+          res.json(thoughtData);
+    } catch(error) {
+        res.status(500).json(error);
+    }
+};
+
+const getSingleThought = async (req,res) => {
+    try {
+        let thoughtData = Thoughts.findOne({_id:req.param.thoughtId});
+        if(!thoughtData) {
+            res.status(404).json({message: 'No thoughts found for this Id  !'});
+        }
+        res.status(200).json(thoughtData);
+    } catch(error) {
+        res.status(500).json(error);
+    }
+};
+
+
+module.exports = {createThought, updateThought,deleteThought, getThoughts, getSingleThought,  }
 
 
 
-getThoughts,
-  getSingleThought,
-  updateThought,
+
+  
   deleteThought,
   addReaction,
-  removeReaction,
+  removeReaction
