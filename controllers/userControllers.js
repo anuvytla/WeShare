802b1978b37095b149c04ef3d8b6d8a85ddcab92
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-09-02 01:04:42 -0700

    debug the user controller file

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,17 +1,19 @@
-const {Users, Thoughts} =  require('../models');
+const {User, Thoughts} =  require('../models');
+
 
 const createUser = async(req,res) => {
     try{
-        let userData = await Users.create(req.body);
+        let userData = await User.create(req.body);
         res.status(200).json(userData);
     } catch(error) {
         res.status(500).json({error});
     }
 };
 
+
 const updateUser = async(req,res) => {
     try {
-        let userData = await Users.findOneAndUpdate(
+        let userData = await User.findOneAndUpdate(
             { 
                 _id: req.params.userId 
             },
@@ -33,9 +35,10 @@ const updateUser = async(req,res) => {
     }
 };
 
+// Delete one user by their _id
 const deleteUser = async(req,res) => {
     try {
-        let userData = await Users.findOneAndDelete(
+        let userData = await User.findOneAndDelete(
             {
                  _id: req.params.userId 
             });
@@ -46,35 +49,33 @@ const deleteUser = async(req,res) => {
 
         let userThought = await Thoughts.deleteMany({ _id: { $in: userData.thoughts } });
         res.json({message: "user has been successfully deleted!"});
-
     } catch {
         res.status(500).json({error});
     }
 };
 
+// get all of the users
 const getUsers = async(req,res) => {
     try {
-        let userData = await Users.findOne(
-            { 
-                _id: req.params.userId 
-            })
+        let userData = await User.find()
         .select("-__v")
+        // Fill objects for friends and thoughts.
         .populate("friends")
         .populate("thoughts")
 
-        if (!userData) {
-            res.status(404).json({ message: "No User find with this ID!" })
-        }
         res.status(200).json(userData);
     } catch (error) {
         res.status(500).json({error});
     }
 };
 
+// Find a single user by their _id.
 const getSingleUser = async(req,res) => {
     try {
-        let userData = await Users.findOne({ _id: req.params.userId })
+        // Find the user by userId param.
+        let userData = await User.findOne({ _id: req.params.userId })
         .select("-__v")
+        // Fill objects for friends and thoughts.
         .populate("friends")
         .populate("thoughts")
         if (!userData) {
@@ -86,9 +87,11 @@ const getSingleUser = async(req,res) => {
     }
 };
 
+// adding the friend  for the user
 const addFriend = async (req,res) => {
     try{
-        let userData = Users.findOneAndUpdate(
+        // Find the user by userId param and add the friendId to list of friends.
+        let userData = await User.findOneAndUpdate(
             { 
                 _id: req.params.userId 
             },
@@ -98,19 +101,25 @@ const addFriend = async (req,res) => {
             { 
                 new: true 
             }
-          );
+          )
+          // Populate friends and thoughts.
+          .populate("friends")
+          .populate("thoughts");
               if (!userData) {
                 return res.status(404).json({ message: "No user with this id!" });
               }
               res.status(200).json(userData);
     } catch(error) {
+        console.log(error);
         res.status(500).json({error});
     }
 };
 
+// deleting the user friend from the user friends list
 const removeFriend = async(req,res) => {
     try{
-        let userData = await Users.findOneAndUpdate(
+        // Find the user by userId param and remove the friendId from list of friends.
+        let userData = await User.findOneAndUpdate(
             { 
                 _id: req.params.userId 
             },
@@ -132,7 +141,7 @@ const removeFriend = async(req,res) => {
 };
 
 
-
+// exports user methods
 module.exports = {createUser, updateUser,deleteUser, getUsers, getSingleUser, addFriend, removeFriend};
 
     
